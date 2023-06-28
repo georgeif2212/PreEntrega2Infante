ddce476ad2315f274c82cbcc4368082f65ab2350
georgeif2212 <2203025256@cua.uam.mx>
2023-06-28 16:14:45 -0600

    Se añade la página de sombreros y estilos al proyecto

--- js/index.js
+++ js/index.js
@@ -1,13 +1,36 @@
+// ! Carrusel html
+document.addEventListener("DOMContentLoaded", function () {
+  new Splide(".splide", {
+    type: "slide", // Cambiar el tipo de carrusel a 'slide'
+    //perPage: 3, // Mostrar solo 1 slide a la vez
+    drag: true, // Habilitar arrastrar
+    dragFree: true, // Habilitar arrastrado libre
+    flickMaxPages: 1, // Arrastrar solo 1 página a la vez
+    easing: "cubic-bezier(.42,0,.58,1)", // Efecto de deslizamiento personalizado
+    perPage: 3, // Mostrar solo 1 elemento
+    perMove: 1, // Mover 1 elemento a la vez
+    breakpoints: {
+      576: {
+        perPage: 1, // Mostrar solo 1 elemento en celular
+      },
+      1023: {
+        perPage: 2, // Mostrar 2 elementos en tablet
+      },
+    },
+  }).mount();
+});
+
 let opcion;
 let total = 0;
 const opcionSalir = 8;
 class Sombrero {
-  constructor(id, color, talla, tipo, precio) {
+  constructor(id, color, talla, tipo, precio,url) {
     this.id = id;
     this.color = color;
     this.talla = talla;
     this.tipo = tipo;
     this.precio = precio;
+    this.url=url;
   }
   describir() {
     console.log(
