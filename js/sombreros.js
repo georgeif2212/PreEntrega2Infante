40b1f9f42fae596a2195922e0e55b617c0010ad2
georgeif2212 <2203025256@cua.uam.mx>
2023-08-02 22:32:58 -0600

    Ahora el input de busqueda funciona

--- js/sombreros.js
+++ js/sombreros.js
@@ -1,4 +1,4 @@
-import {servicios } from "./database.js";
+import { servicios } from "./database.js";
 import {
   obtenerCategoriasSeleccionadas,
   filtrarPorCategorias,
@@ -35,7 +35,95 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 // ! Sombreros
+const categoriasSombreros = [
+  "Caballero",
+  "Niño",
+  "Dama",
+  "Invierno",
+  "Verano",
+  "Bristol",
+  "Bombín",
+  "Turbante",
+  "Chistera",
+  "Floppy",
+];
+const datalist = document.getElementById("categorias-list");
+const searchButton = document.querySelector("#searchButton");
+const selectedCategory = document.querySelector("#selected-category");
 const allProducts = document.querySelector("#all-products");
+
+document.addEventListener("change", () => {
+  searchButton.addEventListener("click", () => {
+    console.log(selectedCategory.value);
+    fetch("../data.json")
+      .then((response) => {
+        return response.json();
+      })
+      .then((response) => {
+        allProducts.innerHTML = "";
+        response.forEach((el) => {
+          if (
+            el.categorias.includes(selectedCategory.value) &&
+            el.seccion == "All"
+          ) {
+            const { precio, url, categorias } = el;
+            const article = document.createElement("article");
+            article.classList.add("producto", "box-product");
+            article.innerHTML = `
+              <img class="producto-img mb-2" src="${url}" alt="" />
+              <div class="producto-info">
+                <h3 class="color-1 size-medium_s producto-info_title pb-1">${categorias[0]}</h3>
+                <p class="color-2 size-small_l producto-info_category mb-0 pb-0">
+                ${categorias[1]}
+                </p>
+                <p class="color-3 size-small_l mb-1 pb-0">${categorias[2]}</p>
+                <div class="producto-info-button">
+                  <button type="button" class="button add-cart">
+                    <span class="button__text">Agregar</span>
+                    <span class="button__icon"
+                      ><svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        viewBox="0 0 24 24"
+                        stroke-width="2"
+                        stroke-linejoin="round"
+                        stroke-linecap="round"
+                        stroke="currentColor"
+                        height="24"
+                        fill="none"
+                        class="svg"
+                      >
+                        <line y2="19" y1="5" x2="12" x1="12"></line>
+                        <line y2="12" y1="12" x2="19" x1="5"></line></svg
+                    ></span>
+                  </button>
+                  <p class="producto-info-button_precio color-2"><b>$ ${precio}</b></B></p>
+                </div>
+              </div>
+            `;
+            const addCartButtons = article.querySelectorAll(".add-cart");
+            addCartButtons.forEach((button) => {
+              button.addEventListener("click", () => {
+                ArrayCarrito.push(el);
+                //localStorage.clear();
+                localStorage.setItem("carrito", JSON.stringify(ArrayCarrito));
+                notiToastify("./carrito.html");
+                updateCart(ArrayCarrito);
+              });
+            });
+            allProducts.appendChild(article);
+          }
+        });
+      });
+  });
+});
+
+categoriasSombreros.forEach((categoria) => {
+  const option = document.createElement("option");
+  option.value = categoria;
+  datalist.appendChild(option);
+});
+
 document.addEventListener("DOMContentLoaded", () => {
   fetch("../data.json")
     .then((response) => {
@@ -94,7 +182,7 @@ document.addEventListener("DOMContentLoaded", () => {
         }
       });
     })
-    .catch((response)=>console.log(response))
+    .catch((response) => console.log(response));
 });
 
 const checkboxes = document.querySelectorAll('[name="check-sombrero"]');
